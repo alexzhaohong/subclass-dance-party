3dcf6401f6cae29f84cd6699f5ab80a2f42ea472
UrAWizardHarryPotter <zach.schwartzman@gmail.com>
2020-03-30 17:57:46 -0700

    First Attempt

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -18,4 +18,33 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   return blinkyDancer;
 };
 
-// COMMENT
+
+
+// var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+//   // var this = Object.create(makeBlinkyDancer.prototype)
+//   // this is instance of BlinkyDancer
+//   makeDancer.call(this, top, left, timeBetweenSteps);
+
+//   // superclass step behavior from Dancer{}
+//   // (already falls back)
+//   this.oldStep = this.step;
+
+//   // return this
+// };
+
+// // all failed lookups will go to makeDancer
+// makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
+// makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+
+// makeBlinkyDancer.prototype.step = function() {
+//   // call the old version of step at the beginning of any call to this new version of step
+//   this.oldStep();
+//   // toggle() is a jQuery method to show/hide the <span> tag.
+//   // See http://api.jquery.com/category/effects/ for this and
+//   // other effects you can use on a jQuery-wrapped html tag.
+//   this.$node.toggle();
+// };
+
+// // Invocation
+// // var blinkyDancer = new makeBlinkyDancer(10, 20, timeBetweenSteps);
+
--- src/dancer.js
+++ src/dancer.js
@@ -1,4 +1,51 @@
 // Creates and returns a new dancer object that can step
+  var makeDancer = function(top, left, timeBetweenSteps) {
+
+  var dancer = Object.create(makeDancer.prototype);
+
+  // use jQuery to create an HTML <span> tag
+  dancer.$node = $('<span class="dancer"></span>');
+
+  dancer.step(timeBetweenSteps);
+
+  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
+  // this one sets the position to some random default point within the body
+  dancer.setPosition(top, left);
+
+  return dancer;
+};
+
+makeDancer.prototype.step = function(timeBetweenSteps) {
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  debugger;
+  setTimeout(step.bind(makeDancer), timeBetweenSteps); //timeBetweenSteps is unfound
+};
+
+// function(){step.call(this)}
+// ===
+// step.bind(this)
+// ===
+// this.step === step.bind(this)
+
+
+makeDancer.prototype.setPosition = function(top, left) {
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);
+};
+
+
+
+
+/* OLD VERSION
+
+// Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps) {
 
   var dancer = {};
@@ -30,3 +77,41 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 
   return dancer;
 };
+*/
+
+// Psuedoclassical
+/*
+// Creates and returns a new dancer object that can step
+var makeDancer = function(top, left, timeBetweenSteps) {
+
+  var dancer = Object.create(makeDancer.prototype);
+
+  // use jQuery to create an HTML <span> tag
+  dancer.$node = $('<span class="dancer"></span>');
+
+  dancer.step(timeBetweenSteps);
+
+  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
+  // this one sets the position to some random default point within the body
+  dancer.setPosition(top, left);
+
+  return dancer;
+};
+
+makeDancer.prototype.step = function(timeBetweenSteps) {
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  setTimeout(step.bind(makeDancer), timeBetweenSteps); //timeBetweenSteps is unfound
+};
+
+makeDancer.prototype.setPosition = function(top, left) {
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);
+};
+ */
